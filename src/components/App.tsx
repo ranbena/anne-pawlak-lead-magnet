b35dee23d2417cc7f26353f61b0273ff58a1a7a6
Ran Byron <ranbena@gmail.com>
2024-02-13 06:20:52 +0100

    redesign

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,79 @@
 import { Form } from './Form';
+import { InstagramFilled, ProfileFilled } from '@ant-design/icons';
+import Tooltip from 'antd/es/tooltip';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <div className="main">
-        <div className="visual" />
-        <div className="text">
-          <div>
-            <h1>Anne Pawlak</h1>
-            <h2>You're in the right place</h2>
-            <Form />
-          </div>
-          <div className="footer">
-            I agree to the privacy policy and automatically register for the
-            Anne Pawlak's newsletter with this submission of my email.
-          </div>
+      <header>
+        <div>
+          <span className="links">
+            <a
+              href="https://smart.bio/fortunes_exploits/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip title="Bio" color="#624dc3">
+                <ProfileFilled />
+              </Tooltip>
+            </a>
+            <a
+              href="https://www.instagram.com/fortunes_exploits"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip title="Instagram" color="#624dc3">
+                <InstagramFilled />
+              </Tooltip>
+            </a>
+          </span>
         </div>
-      </div>
+      </header>
+      <section className="main">
+        <div>
+          <div className="text">
+            <div>
+              <h1>CONTENT HOOKS FOR 2024</h1>
+              <h2>50+ content starters that will make them stop scrolling</h2>
+              <Form />
+            </div>
+          </div>
+          <div className="visual" />
+        </div>
+      </section>
+      <section className="desc">
+        <div>
+          <h3>
+            <em>This pdf</em> is helping you if..
+          </h3>
+          <p>
+            ... you have an idea for online content or creative but don't know
+            how to start or create an engaging beginning.
+            <br />
+            <br />
+            Find out how what makes your followers stop scrolling, increase your
+            views.
+          </p>
+          <p>
+            ... you are afraid that your marketing content is not powerful
+            enough to get your message across with all the competition in 2024.
+            <br />
+            <br />
+            Find out how you can increase the number of your viewers and turn
+            them into followers.
+          </p>
+          <p>
+            ... you have no idea how strategic use of hooks increases your
+            chances of really finding out how your audience ticks so you can
+            turn them into buyers.
+            <br />
+            <br />
+            These proven hook ideas can be used for personality profiling of
+            your audience.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,15 +3,24 @@ import {
   useShortAnswerInput,
   useGoogleForm,
   GoogleFormProvider,
+  useCheckboxInput,
 } from 'react-google-forms-hooks';
 import { Form as AntForm, Button } from 'antd';
 import formData from '../data/GoogleForm.json';
 import { CheckOutlined, SendOutlined } from '@ant-design/icons';
 import './Form.css';
 
-const errorMessages: Record<string, string> = {
-  required: 'Please fill out both fields',
-  pattern: 'Email address invalid',
+const errorMessages: Record<string, { required?: string; pattern?: string }> = {
+  '872857641': {
+    required: 'Please provide your full name',
+  },
+  '1215295443': {
+    required: 'Please provide your email address',
+    pattern: 'Email address invalid',
+  },
+  '1918354548': {
+    required: 'Please check the registration agreement',
+  },
 };
 
 type IFormInputs = {
@@ -48,6 +57,27 @@ const ShortAnswerInput = ({
   );
 };
 
+const CheckboxInput = ({
+  id,
+  disabled,
+}: Pick<IFormInputs, 'id' | 'disabled'>) => {
+  const options = useCheckboxInput(id).options[0];
+
+  return (
+    <AntForm.Item className="antFormItem">
+      <div className="checkbox">
+        <input
+          type="checkbox"
+          id={options.id}
+          {...options.registerOption()}
+          disabled={disabled}
+        />
+        <label htmlFor={options.id}>{options.label}</label>
+      </div>
+    </AntForm.Item>
+  );
+};
+
 const Form = () => {
   // @ts-ignore
   const methods = useGoogleForm({ form: formData });
@@ -64,10 +94,17 @@ const Form = () => {
   const errorMessage = React.useMemo(() => {
     if (!formState.errors) return undefined;
 
-    const arr = Object.values(formState.errors);
-    const type = arr.length ? (arr[0]?.type as string) : undefined;
-    if (type) {
-      return errorMessages[type];
+    const arr = Object.entries(
+      formState.errors as Record<
+        string,
+        {
+          type: 'required' | 'pattern';
+        }
+      >
+    );
+    if (arr.length) {
+      const [id, { type }] = arr[0];
+      return errorMessages[id][type];
     }
   }, [formState]);
 
@@ -77,28 +114,34 @@ const Form = () => {
         onSubmit={methods.handleSubmit(onSubmit)}
         className={success ? 'success' : undefined}
       >
-        <ShortAnswerInput
-          id="872857641"
-          placeholder="Full name"
-          disabled={success}
-        />
-        <ShortAnswerInput
-          id="1215295443"
-          type="email"
-          placeholder="Email address"
-          disabled={success}
-        />
-        {errorMessage ? <div className="error">{errorMessage}</div> : null}
-        <Button
-          className="button"
-          size="large"
-          type="primary"
-          htmlType="submit"
-          loading={formState.isSubmitting}
-          icon={success ? <CheckOutlined /> : <SendOutlined />}
-        >
-          {success ? 'Sent!' : 'Send me the PDF'}
-        </Button>
+        <fieldset>
+          <legend>
+            Fill out this form and you'll receive this pdf by email
+          </legend>
+          <ShortAnswerInput
+            id="872857641"
+            placeholder="Full name"
+            disabled={success}
+          />
+          <ShortAnswerInput
+            id="1215295443"
+            type="email"
+            placeholder="Email address"
+            disabled={success}
+          />
+          <CheckboxInput id="1918354548" disabled={success} />
+          {errorMessage ? <div className="error">{errorMessage}</div> : null}
+          <Button
+            className="button"
+            size="large"
+            type="primary"
+            htmlType="submit"
+            loading={formState.isSubmitting}
+            icon={success ? <CheckOutlined /> : <SendOutlined />}
+          >
+            {success ? 'Sent!' : 'Send me the PDF'}
+          </Button>
+        </fieldset>
       </form>
     </GoogleFormProvider>
   );
